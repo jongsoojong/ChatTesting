6c4438023b094f095bec27a4917f0d96f30483ad
Jongsoo Yoon <jongsoojong@gmail.com>
2016-11-21 12:33:14 -0800

    started auth

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-var socketApp = angular.module('socketApp', []);
+
+var socketApp = angular.module('socketApp', ['ui.router']);
+
+
+//CONTROLLERS
+
 
 socketApp.controller('chatController', function($scope, socket){
   var vm = this;
   vm.msgs = [];
+  vm.friends = [];
+  vm.currentChat = [];
   vm.sendMessage = function() {
     socket.emit('send-message', vm.msg.text);
     vm.msg.text = '';
@@ -12,7 +19,52 @@ socketApp.controller('chatController', function($scope, socket){
     vm.msgs.push(data);
     $scope.$digest();
   });
+
 });
+
+socketApp.controller('signUpController', function($scope, $state, $http){
+  var vm = this;
+
+
+  vm.createUser = function() {
+    if(vm.newUser.username.length <= 6 || vm.newUser.password.length <= 6) {
+      alert("Username or password is too short! ");
+    } else {
+      $http.post('/api/user/signup', vm.newUser)
+      .success(function(res){
+
+      }).error(function(err){
+        console.log(err);
+      })
+    }
+  }
+
+})
+
+//MAKE NAV CONTROLLER
+
+//ROUTERS
+
+socketApp.config(function($stateProvider){
+
+  $stateProvider.state('signUp', {
+    url: "/signup",
+    templateUrl: "./templates/signup.html" ,
+    controller: "signUpController as vm"
+  })
+
+  $stateProvider.state('chatRoom', {
+    url: "/main",
+    templateUrl: "./templates/chatroom.html" ,
+    controller: "chatController as vm"
+  })
+
+
+
+})
+
+
+//FACTORIES (Hellions, Siege Tanks, and Thors! Oh My!)
 
 socketApp.factory('socket', function(){
   var socket = io.connect('http://localhost:8888');
--- server.js
+++ server.js
@@ -5,22 +5,48 @@ var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var User = require('./db/users.js');
+
+//Mongoose!!
+
+mongoose.connect('mongodb://jongsoo:jongsoo@ds017070.mlab.com:17070/jongtest')
+var db = mongoose.connection;
+
+db.once('open', function(){
+  console.log("Power Overwhelming");
+})
 
 
 app.use(express.static("./"))
+app.use(bodyParser.json())
 var port = process.env.PORT || 8888;
 
-// app.get('/', function(req, res){
-//   res.sendFile("index.html");
-// })
+app.get('/', function(req, res){
+  res.sendFile("index.html");
+})
+
+
+
+//AUTH
+
+app.post('/api/user/signup', function(req, res) {
+  console.log(req.body)
+  var user = new User(req.body);
+  user.save()
+  //
+  // res.json(req.body) WHY?!
+})
+
+app.get('/api/user/login', function(req, res){
+  User.findOne({'username': req.body})
+})
+
 
 io.sockets.on('connection', function(socket){
   socket.on('send-message', function(data){
     io.sockets.emit('get-message', data)
   })
 })
-
-
 
 server.listen(port, function(){
   console.log("Conjuring Undead at ", + port)
